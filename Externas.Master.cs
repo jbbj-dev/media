c2f099c9c9e0dbcbc6ec83de00ce95321cfdcd9f
jbbj-dev <jbbj.cadastros@gmail.com>
2026-04-20 22:47:17 -0300

    Add files via upload

---
+++ Externas.Master.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Web;
+
+namespace Ibope.MediaPricing.Web.templates
+{
+    public partial class Externas : System.Web.UI.MasterPage
+    {
+        public static string GetVersaoSistema()
+        {
+            string versaoSistema = string.Empty;
+            string pathArquivoVersao = HttpContext.Current.Server.MapPath("~/versao.txt");
+
+            if (File.Exists(pathArquivoVersao))
+                versaoSistema = File.ReadAllText(pathArquivoVersao);
+            else
+                versaoSistema = "DEV";
+
+            return versaoSistema;
+        }
+
+        public static string GetAnoCorrente()
+        {
+            return DateTime.Now.Year.ToString();
+        }
+    }
+}
---
+++ Internas.Master.cs
@@ -0,0 +1,54 @@
+﻿using Ibope.MediaPricing.Dominio.Servicos.Monitoracao;
+using System;
+using System.IO;
+using System.Web;
+
+namespace Ibope.MediaPricing.Web.templates
+{
+    public partial class Internas : System.Web.UI.MasterPage
+    {
+        private ServicoMonitoracao servicoMonitoracao = new ServicoMonitoracao();
+
+        public static string GetVersaoSistema()
+        {
+            string versaoSistema = string.Empty;
+            string pathArquivoVersao = HttpContext.Current.Server.MapPath("~/versao.txt");
+
+            if (File.Exists(pathArquivoVersao))
+                versaoSistema = File.ReadAllText(pathArquivoVersao);
+            else
+                versaoSistema = "DEV";
+
+            return versaoSistema;
+        }
+
+        public static string GetAnoCorrente()
+        {
+            return DateTime.Now.Year.ToString();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string mensagemAlerta = string.Empty;
+
+            bool primeiraInteracao = true;
+
+            foreach (string item in servicoMonitoracao.MonitoracaoAlerta(DateTime.Now))
+            {
+                if (!primeiraInteracao)
+                    mensagemAlerta += "</br></br>";
+
+                primeiraInteracao = false;
+                mensagemAlerta += item;
+            }
+
+            imgAlerta.Visible = false;
+
+            if (!string.IsNullOrEmpty(mensagemAlerta))
+            {
+                imgAlerta.Alt = mensagemAlerta + "</br></br> " + Dominio.Utilitarios.Config.SiteMensagemMonitoracaoErro;
+                imgAlerta.Visible = true;
+            }
+        }
+    }
+}
